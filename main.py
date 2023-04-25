ab5ad75db6c71df0b3217624c892975782b2098c
mattman2864 <45152940+mattman2864@users.noreply.github.com>
2023-04-25 10:42:52 -0400

    Added armor and gear managament system

--- main.py
+++ main.py
@@ -4,18 +4,57 @@ import json
 with open("items.json") as f:
     items = json.load(f)
 
+class Entity:
+    def __init__(self, health, defense):
+        pass
+
 class Player:
     def __init__(self):
         self.health = 100
+        self.hunger = 100
+        self.defense = 0
         self.inventory = []
+        self.armorSlots = {"helmet":None, "chest":None, "legs":None, "boots":None}
+        self.weaponSlots = {"sword":None, "bow":None}
     def addToInventory(self, item):
         self.inventory.append(item)
         print(f"+1 {item}")
     def displayInventory(self) -> str:
-        invdisp = "Inventory:"
+        inventoryDisplay = "Inventory:"
         for i in self.inventory:
-            invdisp += f"\n   -{i}"
-        return invdisp
+            inventoryDisplay += f"\n   -{i}"
+        return inventoryDisplay
+    def equipArmor(self, item):
+        found = False
+        for i in self.inventory:
+            if i.id == item and i.itemClass in ["helmet", "chest", "legs", "boots"]:
+                found = True
+                self.armorSlots[i.itemClass] = i
+                self.inventory.remove(i)
+    def equipWeapon(self, item):
+        found = False
+        for i in self.inventory:
+            if i.id == item and i.itemClass in ["sword", "bow"]:
+                found = True
+                self.weaponSlots[i.itemClass] = i
+                self.inventory.remove(i)  
+    def equip(self, item):
+        for i in self.inventory:
+            if i.id == item:
+                if i.itemClass in ["sword", "bow"]:
+                    self.equipWeapon(item)
+                if i.itemClass in ["helmet", "chest", "legs", "boots"]:
+                    self.equipArmor(item)
+    def displayEquipment(self):
+        weaponIter = self.weaponSlots.copy()
+        weaponIter.update({"":"",None:""})
+        print("\t{:<30}{}".format("Armor", "Weapons"))
+        for armor, weapon in zip(self.armorSlots, weaponIter):
+            if weapon:
+                print("\t{:<30}{}".format(f"{armor}: {self.armorSlots[armor]}", f"{weapon}: {weaponIter[weapon]}"))
+            else:
+                print("\t{:<30}{}".format(f"{armor}: {self.armorSlots[armor]}", ""))
+
 
 class Item:
     def __init__(self, itemClass, name):
@@ -23,20 +62,31 @@ class Item:
         self.id = name
         self.iteminfo = items[self.itemClass][self.id]
         self.name = self.iteminfo["displayName"]
-
-        for i in self.iteminfo
+        self.description = items[self.itemClass][self.id]["description"]
+        self.stats = {}
+        for i in self.iteminfo:
+            if not i in ["description", "displayName"]:
+                self.stats[i] = self.iteminfo[i]
+        self.displaystats = ""
+        for i in self.stats:
+            self.displaystats += f"{i}: {self.stats[i]} "
 
     def __str__(self) -> str:
-        return f"{self.name} ({self.stats})"
+        return f"{self.name}"
     def getDescription(self):
-        return items[self.itemClass][self.id]["description"]
+        details = f"{self.name}"
+        for i in self.stats:
+            if not i in ["description", "displayName"]:
+                details += f"\n\t{i}: {self.stats[i]}"
+        details += f"\n\t{self.description}"
+        return details
     
 class Sword(Item):
     def __init__(self, name):
         self.itemClass = "sword"
         self.id = name
         self.damage = items[self.itemClass][self.id]["damage"]
-        super().__init__(self.itemClass, name)
+        super().__init__(self.itemClass, self.id)
 
 class Bow(Item):
     def __init__(self, name):
@@ -45,10 +95,41 @@ class Bow(Item):
         self.damage = items[self.itemClass][self.id]["damage"]
         self.reloadTime = items[self.itemClass][self.id]["reloadTime"]
         super().__init__(self.itemClass, name)
+
+class Helmet(Item):
+    def __init__(self, name):
+        self.itemClass = "helmet"
+        self.id = name
+        self.defense = items[self.itemClass][self.id]["defense"]
+        super().__init__(self.itemClass, name)
+
+class Chestplate(Item):
+    def __init__(self, name):
+        self.itemClass = "chest"
+        self.id = name
+        self.defense = items[self.itemClass][self.id]["defense"]
+        super().__init__(self.itemClass, name)
+
+class Leggings(Item):
+    def __init__(self, name):
+        self.itemClass = "legs"
+        self.id = name
+        self.defense = items[self.itemClass][self.id]["defense"]
+        super().__init__(self.itemClass, name)
+
+class Boots(Item):
+    def __init__(self, name):
+        self.itemClass = "boots"
+        self.id = name
+        self.defense = items[self.itemClass][self.id]["defense"]
+        super().__init__(self.itemClass, name)
     
 class Food(Item):
-    def __init__(self, name, calories):
-        super().__init__(name, f" {calories}")
+    def __init__(self, name):
+        self.itemClass = "food"
+        self.id = name
+        self.calories = items[self.itemClass][self.id]["calories"]
+        super().__init__(self.itemClass, self.id)
 
 class Game:
     def __init__(self, player):
@@ -56,7 +137,9 @@ class Game:
                                ["move", ["dir"], "search around the area"],
                                ["inv", [""],"displays inventory"],
                                ["desc", ["item"], "shows more details about an item"],
-                               ["get", ["item"], "puts item into your inventory"]
+                               ["get", ["item"], "puts item into your inventory"],
+                               ["equip", ["item"], "equip weapon or armor piece - will replace armor in slot"],
+                               ["gear", [""], "displays equipment"]
                                ]
         self.running = True
         self.availableCommands = self.commandOptions.copy()
@@ -95,11 +178,31 @@ class Game:
             if args[0] == "sword":
                 self.player.addToInventory(Sword("iron_sword"))
             elif args[0] == "bow":
-                self.player.addToInventory(Bow("kings_bow"))
+                self.player.addToInventory(Bow("soldiers_bow"))
+            elif args[0] == "food":
+                self.player.addToInventory(Food("apple"))
+            elif args[0] == "helmet":
+                self.player.addToInventory(Helmet("iron_helm"))
+            elif args[0] == "chest":
+                self.player.addToInventory(Chestplate("iron_chestplate"))
+            elif args[0] == "legs":
+                self.player.addToInventory(Leggings("iron_leggings"))
+            elif args[0] == "boots":
+                self.player.addToInventory(Boots("iron_boots"))
         if command == "desc":
+            found = False
             for i in self.player.inventory:
                 if i.id == args[0]:
-                    pass
+                    print(i.getDescription())
+                    found = True
+            if (not found) and args:
+                print(f"You do not have {args[0]} in your inventory.")
+            else:
+                print("You must give an item to find the description of")
+        if command == "equip":
+            self.player.equip(args[0])
+        if command == "gear":
+            self.player.displayEquipment()
 
 if __name__ == "__main__":
     player = Player()
